dc943546c8d6c5a29b3f46c5ba41bd1c8590d76a
frustratedcat <brownjared205@gmail.com>
2024-10-06 20:41:11 -0400

    Fully style home page

--- script.js
+++ script.js
@@ -3,35 +3,32 @@ function header() {
   let html = `
     <header>
       <nav>
-        <ul>
-          <li><a href="${
+        <ul class="navigation">
+          <li class="nav-to-home"><a href="${
             document.title === "Frustrated Cat"
               ? "./index.html"
               : "../../index.html"
           }">Frustrated Cat</a></li>
-          <li><a href="${
+          <li class="li-gap"><a href="${
             document.title === "Frustrated Cat"
               ? "./pages/fiction/fiction.html"
               : "../../pages/fiction/fiction.html"
           }">Fiction</a></li>
-          <li><a href="${
+          <li class="li-gap"><a href="${
             document.title === "Frustrated Cat"
               ? "./pages/articles/articles.html"
               : "../../pages/articles/articles.html"
           }">Articles</a></li>
-          <li><a href="${
+          <li class="li-gap"><a href="${
             document.title === "Frustrated Cat"
               ? "./pages/photography/photography.html"
               : "../../pages/photography/photography.html"
           }">Photography</a></li>
-          <li><a href="${
+          <li class="li-gap"><a href="${
             document.title === "Frustrated Cat"
               ? "./pages/reading-list/reading-list.html"
               : "../../pages/reading-list/reading-list.html"
           }">Reading List</a></li>
-          <li><button><a href="https://www.instagram.com/jaredreadsandruns" target="blank"><span>Instagram</span></a></button></li>
-          <li><button><a href="https://www.github.com/frustratedcat" target="blank"><span>Github</span></a></button></li>
-          <li><button>dark/light</button></li>
         </ul>
       </nav>
     </header>
